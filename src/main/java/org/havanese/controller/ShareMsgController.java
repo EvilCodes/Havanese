b95bbfe70f3bdc469ffbdadd7245da91b380b1c7
blackfox <huangy_tao@126.com>
2018-12-14 16:15:59 +0800

    分享界面跳转逻辑的实现

---
+++ src/main/java/org/havanese/controller/ShareMsgController.java
@@ -0,0 +1,18 @@
+package org.havanese.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2018/12/14 0014.
+ */
+@Controller
+@RequestMapping("havanese/common")
+public class ShareMsgController {
+    @RequestMapping("sharemsg")
+    private String goToCurrentPage(){
+        return "jie/index";
+    }
+
+
+}
