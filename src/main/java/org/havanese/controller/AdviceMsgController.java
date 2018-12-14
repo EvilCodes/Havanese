6557a2e2bc29550a5b9d426df0f85b58bf8cb740
blackfox <huangy_tao@126.com>
2018-12-14 16:22:45 +0800

    建议界面跳转逻辑实现

---
+++ src/main/java/org/havanese/controller/AdviceMsgController.java
@@ -0,0 +1,16 @@
+package org.havanese.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2018/12/14 0014.
+ */
+@Controller
+@RequestMapping("havanese/common")
+public class AdviceMsgController {
+    @RequestMapping("advice")
+    private String goToCurrentPage(){
+        return "jie/index";
+    }
+}
