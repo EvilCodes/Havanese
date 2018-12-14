15c25b9e171429a0afffd4cced4cb048bc8ad99d
blackfox <huangy_tao@126.com>
2018-12-14 14:49:47 +0800

    添加根URL并设置控制器注解

--- src/main/java/org/havanese/controller/GetDetailMsgController.java
+++ src/main/java/org/havanese/controller/GetDetailMsgController.java
@@ -1,7 +1,12 @@
 package org.havanese.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Administrator on 2018/12/14 0014.
  */
+@Controller
+@RequestMapping("havanese")
 public class GetDetailMsgController {
 }
