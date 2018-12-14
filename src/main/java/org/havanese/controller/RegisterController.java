9ca74fecbeb3063e61a2df235398847cc02a5abd
blackfox <huangy_tao@126.com>
2018-12-14 15:32:43 +0800

    用户注册界面跳转逻辑实现

--- src/main/java/org/havanese/controller/RegisterController.java
+++ src/main/java/org/havanese/controller/RegisterController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Administrator on 2018/12/14 0014.
  */
 @Controller
-@RequestMapping("havanese")
+@RequestMapping("havanese/user")
 public class RegisterController {
+    @RequestMapping("register")
+    private String goToCurrentPage(){
+        return "user/reg";
+    }
 }
