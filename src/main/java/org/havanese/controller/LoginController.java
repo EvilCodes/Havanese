03a98f6b5de5366c5bf39a74ed998cb90b375eac
blackfox <huangy_tao@126.com>
2018-12-14 15:34:33 +0800

    用户登陆界面跳转逻辑实现

--- src/main/java/org/havanese/controller/LoginController.java
+++ src/main/java/org/havanese/controller/LoginController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Administrator on 2018/12/14 0014.
  */
 @Controller
-@RequestMapping("havanese")
+@RequestMapping("havanese/user")
 public class LoginController {
+    @RequestMapping("login")
+    private String goToCurrentPage(){
+        return "user/login";
+    }
 }
