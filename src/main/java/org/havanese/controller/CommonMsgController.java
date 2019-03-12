d90142879e7e81156a44d3121e71b1b492657bbd
blackfox <huangy_tao@126.com>
2019-03-12 15:20:19 +0800

    公告已登录状态子页面跳转逻辑的实现

--- src/main/java/org/havanese/controller/CommonMsgController.java
+++ src/main/java/org/havanese/controller/CommonMsgController.java
@@ -1,5 +1,6 @@
 package org.havanese.controller;
 
+import org.havanese.util.SubPageJumping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("havanese/common")
 public class CommonMsgController {
+    private static final String BASE_DESC="announcement";
+    private String pathDesc;
     @RequestMapping("announcement")
     private String goToCurrentPage(){
         return "jie/announcement";
@@ -17,5 +20,49 @@ public class CommonMsgController {
     @RequestMapping("loginedAnnouncement")
     private String goToLoginedCurrentPage(){
         return "jie/logined/announcement";
+    }
+
+    @RequestMapping("loginedAnnouncement/essence")
+    private String goToLoginedEssencePage(){
+        pathDesc = SubPageJumping.essencePageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+
+    @RequestMapping("loginedAnnouncement/payamount")
+    private String goToLoginedPayamountPage(){
+        pathDesc = SubPageJumping.payamountPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+    @RequestMapping("loginedAnnouncement/synthesis")
+    private String goToLoginedSynthesisPage(){
+        pathDesc = SubPageJumping.synthesisPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+    @RequestMapping("loginedAnnouncement/unliquidated")
+    private String goToLoginedUnliquidatedPage(){
+        pathDesc = SubPageJumping.unliquidatedPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+
+    @RequestMapping("announcement/essence")
+    private String goToEssencePage(){
+        pathDesc = SubPageJumping.essencePageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+
+    @RequestMapping("announcement/payamount")
+    private String goToPayamountPage(){
+        pathDesc = SubPageJumping.payamountPageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+    @RequestMapping("announcement/synthesis")
+    private String goToSynthesisPage(){
+        pathDesc = SubPageJumping.synthesisPageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+    @RequestMapping("announcement/unliquidated")
+    private String goToUnliquidatedPage(){
+        pathDesc = SubPageJumping.unliquidatedPageDesc(BASE_DESC,false);
+        return pathDesc;
     }
 }
