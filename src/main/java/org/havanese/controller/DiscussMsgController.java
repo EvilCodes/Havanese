8a1c57000fc0a235bd7be4bf4a328116279ffed5
blackfox <huangy_tao@126.com>
2019-03-12 11:56:19 +0800

    讨论子页面已登录状态页面跳转逻辑的实现

--- src/main/java/org/havanese/controller/DiscussMsgController.java
+++ src/main/java/org/havanese/controller/DiscussMsgController.java
@@ -1,5 +1,6 @@
 package org.havanese.controller;
 
+import org.havanese.util.SubPageJumping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("havanese/common")
 public class DiscussMsgController {
+    private static final String BASE_DESC = "discuss";
+    private String pathDesc = null;
     @RequestMapping("discuss")
     private String goToCurrentPage(){
         return "jie/discuss";
@@ -18,4 +21,50 @@ public class DiscussMsgController {
     private String goToLoginedCurrentPage(){
         return "jie/logined/discuss";
     }
+
+    @RequestMapping("loginedDiscuss/essence")
+    private String goToLoginedEssencePage(){
+        pathDesc = SubPageJumping.essencePageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+
+    @RequestMapping("loginedDiscuss/payamount")
+    private String goToLoginedPayamountPage(){
+        pathDesc = SubPageJumping.payamountPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+    @RequestMapping("loginedDiscuss/synthesis")
+    private String goToLoginedSynthesisPage(){
+        pathDesc = SubPageJumping.synthesisPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+    @RequestMapping("loginedDiscuss/unliquidated")
+    private String goToLoginedUnliquidatedPage(){
+        pathDesc = SubPageJumping.unliquidatedPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+
+    @RequestMapping("discuss/essence")
+    private String goToEssencePage(){
+        pathDesc = SubPageJumping.essencePageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+
+    @RequestMapping("discuss/payamount")
+
+    private String goToPayamountPage(){
+        pathDesc = SubPageJumping.payamountPageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+    @RequestMapping("discuss/synthesis")
+    private String goToSynthesisPage(){
+        pathDesc = SubPageJumping.synthesisPageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+    @RequestMapping("discuss/unliquidated")
+    private String goToUnliquidatedPage(){
+        pathDesc = SubPageJumping.unliquidatedPageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+
 }
