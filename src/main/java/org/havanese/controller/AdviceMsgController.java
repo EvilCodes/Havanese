8d7cc4c9c89856038b3ffcf14b520de828a188ba
blackfox <huangy_tao@126.com>
2019-03-12 11:08:40 +0800

    建议子页面已登录跳转逻辑的实现

--- src/main/java/org/havanese/controller/AdviceMsgController.java
+++ src/main/java/org/havanese/controller/AdviceMsgController.java
@@ -1,5 +1,6 @@
 package org.havanese.controller;
 
+import org.havanese.util.SubPageJumping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("havanese/common")
 public class AdviceMsgController {
+    private String BASE_DESC = "advice";
+    private String pathDesc = null;
     @RequestMapping("advice")
     private String goToCurrentPage(){
         return "jie/advice";
@@ -19,4 +22,54 @@ public class AdviceMsgController {
 
         return "jie/logined/advice";
     }
+
+    @RequestMapping("loginedAdvice/essence")
+    private String goToLoginedEssencePage(){
+        pathDesc = SubPageJumping.essencePageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+
+    @RequestMapping("loginedAdvice/payamount")
+    private String goToLoginedPayamountPage(){
+        pathDesc = SubPageJumping.payamountPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+    @RequestMapping("loginedAdvice/synthesis")
+    private String goToLoginedSynthesisPage(){
+        pathDesc = SubPageJumping.synthesisPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+    @RequestMapping("loginedAdvice/unliquidated")
+    private String goToLoginedUnliquidatedPage(){
+        pathDesc = SubPageJumping.unliquidatedPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+
+    @RequestMapping("advice/essence")
+    private String goToEssencePage(){
+        pathDesc = SubPageJumping.essencePageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+
+    @RequestMapping("advice/payamount")
+
+    private String goToPayamountPage(){
+        pathDesc = SubPageJumping.payamountPageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+    @RequestMapping("advice/synthesis")
+    private String goToSynthesisPage(){
+        pathDesc = SubPageJumping.synthesisPageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+    @RequestMapping("advice/unliquidated")
+    private String goToUnliquidatedPage(){
+        pathDesc = SubPageJumping.unliquidatedPageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+
+
+
+
+
 }
