421e855e96251427a2dcc90b0a55a5e05a07623c
blackfox <huangy_tao@126.com>
2019-03-12 15:37:38 +0800

    动态已登录状态子页面跳转逻辑的实现

--- src/main/java/org/havanese/controller/DynamicMsgController.java
+++ src/main/java/org/havanese/controller/DynamicMsgController.java
@@ -1,5 +1,6 @@
 package org.havanese.controller;
 
+import org.havanese.util.SubPageJumping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("havanese/common")
 public class DynamicMsgController {
+    private static final String BASE_DESC = "dynamic";
+    private String pathDesc;
     @RequestMapping("dynamic")
     private String goToCurrentPage(){
         return "jie/dynamic";
@@ -17,6 +20,50 @@ public class DynamicMsgController {
     @RequestMapping("loginedDynamic")
     private String goToLoginedCurrent(){
         return "jie/logined/dynamic";
+    }
+    @RequestMapping("loginedDynamic/essence")
+    private String goToLoginedEssencePage(){
+        pathDesc = SubPageJumping.essencePageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+
+    @RequestMapping("loginedDynamic/payamount")
+    private String goToLoginedPayamountPage(){
+        pathDesc = SubPageJumping.payamountPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+    @RequestMapping("loginedDynamic/synthesis")
+    private String goToLoginedSynthesisPage(){
+        pathDesc = SubPageJumping.synthesisPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+    @RequestMapping("loginedDynamic/unliquidated")
+    private String goToLoginedUnliquidatedPage(){
+        pathDesc = SubPageJumping.unliquidatedPageDesc(BASE_DESC,true);
+        return pathDesc;
+    }
+
+    @RequestMapping("dynamic/essence")
+    private String goToEssencePage(){
+        pathDesc = SubPageJumping.essencePageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+
+    @RequestMapping("dynamic/payamount")
+
+    private String goToPayamountPage(){
+        pathDesc = SubPageJumping.payamountPageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+    @RequestMapping("dynamic/synthesis")
+    private String goToSynthesisPage(){
+        pathDesc = SubPageJumping.synthesisPageDesc(BASE_DESC,false);
+        return pathDesc;
+    }
+    @RequestMapping("dynamic/unliquidated")
+    private String goToUnliquidatedPage(){
+        pathDesc = SubPageJumping.unliquidatedPageDesc(BASE_DESC,false);
+        return pathDesc;
     }
 
 }
